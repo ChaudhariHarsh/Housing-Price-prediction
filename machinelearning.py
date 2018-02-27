6f12bc5e0770ff18a7c1cf203b5058d6eb0a5bc6
ChaudhariHarsh <harshchaudhari@rocketmail.com>
2018-02-27 12:07:27 +0530

    Add files via upload
    Machine learning function with minimum error.

--- machinelearning.py
+++ machinelearning.py
@@ -28,9 +28,9 @@ def initialize_parameters(n_x,method):
 
 def feature_normalization(X):
     (row, col) = X.shape
-    for f in range(row):
-        X[f,:] = (X[f,:]- min(X[f,:]))/(max(X[f,:])- min(X[f,:]))
-    print X
+    for f in range(1,row):
+        X[f,:] = (X[f,:]- min(X[f,:].T))/(max(X[f,:].T)- min(X[f,:].T))
+    assert(X.shape==(row,col)),"Error in size match : feature_normalization"
     return X
 
 def cost_function(X, Y, W, b, method):
@@ -38,11 +38,12 @@ def cost_function(X, Y, W, b, method):
     (n,m) = X.shape
     if method == "Linear":
         hyponthsis_function = np.dot(W,X) + b
-        cost = (hyponthsis_function - Y)**2
-        error = np.sum(cost /(2*m))
+        cost = np.square(hyponthsis_function - Y)
+        error = np.sum(cost /(m),axis=1)
     elif method == "Logistic":
-        hyponthsis_function = np.dot(W,X) + b
-        cost = 1/(1+exp(-hyponthsis_function))
+        Z = np.dot(W,X) + b
+        hyponthsis_function = 1/(1+exp(-hyponthsis_function))
+        cost = - np.dot(Y,np.log(hyponthsis_function).T) - np.dot(1 - Y,np.log(1 - hyponthsis_function).T) 
         error = None
     else:
         print "Error In Cost Function : No method Found"
@@ -53,9 +54,9 @@ def gradient_descent(X, Y, W, b, itertions, learning_rate, method):
     (n,m) = X.shape
     for iteration in range(itertions):
         if method == "Linear":
-            dJ = np.dot(W,X) + b
-            db = np.sum(dJ, axis=1)/(2*m)
-            dW = np.sum(np.dot(dJ,X), axis=1)/(2*m)
+            dJ = np.dot(W,X) + b - Y
+            db = np.sum(dJ, axis=0)/(2*m)
+            dW = np.sum(np.dot(dJ,X.T), axis=0)/(2*m)
         elif method == "Logistic":
             dJ = None
             db = None
@@ -65,12 +66,13 @@ def gradient_descent(X, Y, W, b, itertions, learning_rate, method):
 
         W = W - learning_rate * dW
         b = b - learning_rate * db
-
-        
-
+    print dJ.shape
+    assert(dJ.shape == (W.shape[0],X.shape[1]))
+    assert(db.shape == b.shape)
+    assert(dW.shape == W.shape)
     return W,b
 
-def linear_regression(X, Y, itertions, learning_rate, method):
+def linear_regression(X, Y, itertions, learning_rate, method = "Linear"):
     
     (row, col) = X.shape
 
@@ -109,7 +111,6 @@ for line in file:
     
 Z = np.matrix(Z, dtype=float)
 (j, k) = Z.shape
-print Z[5,:]
 X = np.zeros((j,k+2),dtype=float)
 Y = np.zeros((j,1),dtype=float)
 for i in range(j):
@@ -121,6 +122,9 @@ for i in range(j):
     Y[i,0]=Z[i,2]
 X = np.matrix(X)
 X = X.T
+Y = Y.T
 
-hponthsis_function, error = linear_regression(X, Y, itertions=100, learning_rate=0.000000001, method="Linear")
-
+hyponthsis_function, error = linear_regression(X, Y, itertions=100000, learning_rate=0.01, method="Linear")
+##h = hyponthsis_function.T
+print Y[0,5], hyponthsis_function[0,5]
+print error
